d19f9e6cd14a13f1090aaf3baaf71b9f57dbc478
Vicente Adolfo Bolea Sanchez <vicente.bolea@gmail.com>
2018-06-09 14:16:53 +0900

    Changes in estimator

--- estimator_keras.py
+++ estimator_keras.py
@@ -11,11 +11,12 @@ from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten
 from keras.layers import Conv2D, MaxPooling2D
 from keras import backend as K
+from IPython import embed
 import h5py as h5
 
-batch_size = 128
-num_classes = 10
-epochs = 12
+batch_size = 128 
+num_classes = 25000 
+epochs = 10
 
 # input image dimensions
 img_rows, img_cols = 128, 128
@@ -25,11 +26,11 @@ TEST_PATH = "/scratch/vicente/65000_images.hdf5"
 def load_data():
     train = h5.File(TRAIN_PATH)
 
-    train_y = train['Y'][:]
-    train_x = train['X'][:]
+    train_y = train['Y'][40000:]
+    train_x = train['X'][40000:]
 
-    test_y = train_y
-    test_x = train_x
+    test_y = train_y[20000:]
+    test_x = train_x[20000:]
 
     return (train_x, train_y), (test_x, test_y)
 
@@ -37,18 +38,18 @@ def load_data():
 (x_train, y_train), (x_test, y_test) = load_data()
 
 if K.image_data_format() == 'channels_first':
-    x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
-    x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
-    input_shape = (1, img_rows, img_cols)
+    x_train = x_train.reshape(x_train.shape[0], 3, img_rows, img_cols)
+    x_test = x_test.reshape(x_test.shape[0], 3, img_rows, img_cols)
+    input_shape = (3, img_rows, img_cols)
 else:
-    x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
-    x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
-    input_shape = (img_rows, img_cols, 1)
+    x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 3)
+    x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 3)
+    input_shape = (img_rows, img_cols, 3)
 
 x_train = x_train.astype('float32')
 x_test = x_test.astype('float32')
-x_train /= 255
-x_test /= 255
+#x_train /= 255
+#x_test /= 255
 print('x_train shape:', x_train.shape)
 print(x_train.shape[0], 'train samples')
 print(x_test.shape[0], 'test samples')
@@ -67,7 +68,8 @@ model.add(Dropout(0.25))
 model.add(Flatten())
 model.add(Dense(128, activation='relu'))
 model.add(Dropout(0.5))
-model.add(Dense(num_classes, activation='softmax'))
+#model.add(Dense(num_classes, activation='softmax'))
+model.add(Dense(num_classes, activation='relu'))
 
 model.compile(loss=keras.losses.categorical_crossentropy,
               optimizer=keras.optimizers.Adadelta(),
@@ -79,5 +81,6 @@ model.fit(x_train, y_train,
           verbose=1,
           validation_data=(x_test, y_test))
 score = model.evaluate(x_test, y_test, verbose=0)
-print('Test loss:', score[0])
-print('Test accuracy:', score[1])
+embed()
+print('Test loss:', score)
+print('Test accuracy:', score)
